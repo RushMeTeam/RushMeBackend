0e8421b0ee408d5c8dce52aacc40e2ac68df879a
Andrew Dawson <dawsoa@rpi.edu>
2018-11-27 21:52:15 +0000

    Implemented dropdown data from backend

--- Website Code/my-app/src/App.js
+++ Website Code/my-app/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import rushMeLogo from './rushMeLogo.png'
 import './App.css';
-import {returnCorrectPassword, returnCorrectUsername} from './rushMePasswords.js';
+import {returnCorrectPassword, returnCorrectUsername, returnCorrectKey, returnCorrectSecret} from './rushMePasswords.js';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+var AWS = require('aws-sdk');
+
+
+const AWSKey = returnCorrectKey();
+const AWSSecretKey = returnCorrectSecret();
+
+AWS.config.update({
+  region: "us-east-2",
+  accessKeyId: AWSKey, 
+  secretAccessKey: AWSSecretKey
+});
 
 const options = [
   { value: 'one', label: 'LXA' },
   { value: 'two', label: 'CHP'}
 ];
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 const defaultOption = options[0];
 
@@ -19,15 +32,57 @@ class App extends Component {
     this.state = {
         password : '',
         uname : '',
-        page : 'password'
+        page : 'password',
+        fratData : [{}]
     };
 
 
     this.updatePassword = this.updatePassword.bind(this);
     this.updateUname = this.updateUname.bind(this);
     this.handlePasswordSubmit = this.handlePasswordSubmit.bind(this);
-}
-
+    this.getFraternityData = this.getFraternityData.bind(this);
+    this.getFraternityData();
+  }
+  readTextFile(file)
+  {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status === 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+  }
+  
+  
+  getFraternityData(){
+    var params = {
+      TableName: "FraternityInfo"
+    };
+    if (params != null){
+      console.log(params);
+    }
+    
+    docClient.scan(params,(err, data) => {
+      if(err){
+        console.log(err);
+      } else{
+        var items = data.Items;
+        for(let i = 0; i < items.length; i++) {
+          items[i].label = items[i].namekey;
+        }
+        this.setState({fratData : data.Items})
+        console.log(data.Items);
+      }
+    });
+  }
   handlePasswordSubmit(event){
     if ((this.state.password === returnCorrectPassword()) && (this.state.uname === returnCorrectUsername())){
       console.log("Correct input");
@@ -54,27 +109,25 @@ class App extends Component {
           <h1>
             RushMe Admin Portal
           </h1>
-          <form onSubmit={this.handlePasswordSubmit}>
+        <form onSubmit={this.handlePasswordSubmit}>
           <label>
             Username: <input type="text" value={this.state.uname}  onChange={this.updateUname}></input><br/>
-            Password: <input type="password" value={this.state.password}  onChange={this.updatePassword} ></input><br/>
+            Password:  <input type="password" value={this.state.password}  onChange={this.updatePassword} ></input><br/>
             <input type="submit" value="Submit " />
           </label>
-          </form>
-          <br/>
-          
+        </form>
         </header>
       </div>
     );
     } else {
-      return( 
+      console.log("HERE:");
+      console.log(this.state.fratData);
+      return(
       <div className="App">
-        <header className="App-header">
-          <h1>
-            RushMe Admin Portal Edit Page
-          </h1>
-        </header>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <h1>
+          RushMe Admin Portal Edit Page
+        </h1>
+        <Dropdown options={this.state.fratData} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
       </div> 
     
     );
