3d4e73e8d1deb421cb4d1689d11eab9143ad45c5
Andrew Dawson <dawsoa@rpi.edu>
2018-11-28 19:56:42 +0000

    Added text forms for all inputs in fraternity edit mode."

--- Website Code/my-app/src/App.js
+++ Website Code/my-app/src/App.js
@@ -16,14 +16,9 @@ AWS.config.update({
   secretAccessKey: AWSSecretKey
 });
 
-const options = [
-  { value: 'one', label: 'LXA' },
-  { value: 'two', label: 'CHP'}
-];
-
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-const defaultOption = options[0];
+const defaultOption = {label : 'Please Select a Fraternity to Edit'};
 
 class App extends Component {
   constructor(props) {
@@ -33,6 +28,7 @@ class App extends Component {
         password : '',
         uname : '',
         page : 'password',
+        selectedFraternity : 'NONE',
         fratData : [{}]
     };
 
@@ -41,6 +37,10 @@ class App extends Component {
     this.updateUname = this.updateUname.bind(this);
     this.handlePasswordSubmit = this.handlePasswordSubmit.bind(this);
     this.getFraternityData = this.getFraternityData.bind(this);
+    this.fratSelected = this.fratSelected.bind(this);
+    this.getFraternityObject = this.getFraternityObject.bind(this);
+    this.updateNewCoordinates = this.updateNewCoordinates.bind(this);
+    this.handleFratUpdateSubmit = this.handleFratUpdateSubmit.bind(this);
     this.getFraternityData();
   }
   readTextFile(file)
@@ -74,15 +74,22 @@ class App extends Component {
       if(err){
         console.log(err);
       } else{
-        var items = data.Items;
+        var items = data.Items.sort(function(a,b) {
+          var keyA = a.namekey;
+          var keyB = b.namekey;
+          if(keyA < keyB) return -1;
+          if(keyA > keyB) return 1;
+          return 0;
+        });
         for(let i = 0; i < items.length; i++) {
           items[i].label = items[i].namekey;
         }
-        this.setState({fratData : data.Items})
+        this.setState({fratData : items})
         console.log(data.Items);
       }
     });
   }
+  
   handlePasswordSubmit(event){
     if ((this.state.password === returnCorrectPassword()) && (this.state.uname === returnCorrectUsername())){
       console.log("Correct input");
@@ -97,6 +104,35 @@ class App extends Component {
   
   updateUname(event){
     this.setState({uname : event.target.value});
+  }
+  updateNewCoordinates(event){
+    this.setState({coordinates : event.target.value});
+  }
+  
+  fratSelected(event) {
+    console.log(event.label);
+    this.setState({ page : 'fraternityEdit' });
+    this.setState({ selectedFraternity : event.label });
+    console.log(this.state.fratSelected);
+    
+  }
+  
+  getFraternityObject(){
+    let namekey = this.state.selectedFraternity;
+    let data = this.state.fratData;
+    let desiredFraternityInfo = null;
+    for(let i = 0; i < data.length; i++) {
+      //Namekey matches, set return value and break.
+      if (data[i].namekey === namekey) {
+        desiredFraternityInfo = data[i];
+        break;
+      }
+    }
+    return desiredFraternityInfo;
+  }
+  
+  handleFratUpdateSubmit() {
+    
   }
   
   render() {
@@ -119,15 +155,73 @@ class App extends Component {
         </header>
       </div>
     );
-    } else {
-      console.log("HERE:");
-      console.log(this.state.fratData);
+    } else if (this.state.page === 'main') {
       return(
       <div className="App">
         <h1>
           RushMe Admin Portal Edit Page
         </h1>
-        <Dropdown options={this.state.fratData} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <Dropdown options={this.state.fratData} onChange={this.fratSelected} value={defaultOption} placeholder="Select an option" />
+      </div> 
+    
+    );
+    } else if (this.state.page === 'fraternityEdit') {
+      var data = this.getFraternityObject();
+      return(
+      <div className="App">
+        <h1>
+          RushMe Admin Portal
+        </h1>
+        <h1>
+          You are editing {this.state.selectedFraternity}'s Fraternity Data
+          Submit any field to update it.
+        </h1>
+        <h3>
+          Current Chapter(Alpha Chapter of LCA for example):
+        </h3>
+        <form onSubmit={this.handleCoordinatesSubmit}>
+          <label>
+            <input type="text" value={data.chapter}  onChange={this.updateNewCoordinates}></input><br/>
+            <input type="submit" value="Submit " />
+          </label>
+        </form>
+        <h3>
+          Current Member Count:
+        </h3>
+        <form onSubmit={this.handleCoordinatesSubmit}>
+          <label>
+            <input type="text" value={data.member_count}  onChange={this.updateNewCoordinates}></input><br/>
+            <input type="submit" value="Submit " />
+          </label>
+        </form>
+        <h3>
+          Current Address:
+        </h3>
+        <form onSubmit={this.handleCoordinatesSubmit}>
+          <label>
+            <input type="text" value={data.address}  onChange={this.updateNewCoordinates}></input><br/>
+            <input type="submit" value="Submit " />
+          </label>
+        </form>
+        <h3>
+          Current Description: 
+        </h3>
+        <form onSubmit={this.handleCoordinatesSubmit}>
+          <label>
+            <textarea name="text" rows="10" cols="150" wrap="soft" value={data.description} onChange={this.updateNewCoordinates}> </textarea> <br/>
+            <input type="submit" value="Submit " />
+          </label>
+        </form>
+        <h3>
+          Current Coordinates 
+          (form lattitude,longitude): 
+        </h3>
+        <form onSubmit={this.handleCoordinatesSubmit}>
+          <label>
+            <input type="text" style={{width:'750px'}} value={data.coordinates}  onChange={this.updateNewCoordinates}></input><br/>
+            <input type="submit" value="Submit " />
+          </label>
+        </form>
       </div> 
     
     );
