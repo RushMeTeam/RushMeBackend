d65487a409f25a5629725a3d0a6399e386809fb2
Adam <adamthk@gmail.com>
2019-03-22 13:23:26 -0400

    Finesses Organization table

--- RushMeAdmin/app/controllers/banner.controller.js
+++ RushMeAdmin/app/controllers/banner.controller.js
@@ -1,48 +1,48 @@
 // File: frat-table.controller.js	
 // Description: frat-table controller that will handle all logic for the frat-table.html page	
 
- angular.module('RushMeAdminControllers').controller('BannerCtrl', ['$scope', '$http', function ($scope, $http) {  	
+angular.module('RushMeAdminControllers').controller('BannerCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.email;
   $scope.groups;
   $scope.display_group;
   $scope.committee;
   $http.get("/in/users/current/").then(
-    function(res){
+    function (res) {
       $scope.email = res.data.email;
-    // If the connection between the car and the grandfather
+      // If the connection between the car and the grandfather
       // could be stronger
 
     },
-    function(err){
+    function (err) {
       //Do something with the error here
       console.log("ERR: " + err);
     });
-    $http.get("/in/users/current/groups").then(
-      function(res){
-        $scope.display_group = res.data[0];
+  $http.get("/in/users/current/groups").then(
+    function (res) {
+      $scope.display_group = res.data[0];
       // If the connection between the car and the grandfather
-        // could be stronger
-  
-      },
-      function(err){
-        //Do something with the error here
-        console.log("ERR: " + err);
-      });
-    $http.get("/in/users/current/committee/").then(
-      function(res){
-        $scope.committee = res.data;
+      // could be stronger
+
+    },
+    function (err) {
+      //Do something with the error here
+      console.log("ERR: " + err);
+    });
+  $http.get("/in/users/current/committee/").then(
+    function (res) {
+      $scope.committee = res.data;
       // If the connection between the car and the grandfather
-        // could be stronger
-  
-      },
-      function(err){
-        //Do something with the error here
-        console.log("ERR: " + err);
-      });
-  
-    
-  
-  
+      // could be stronger
+
+    },
+    function (err) {
+      //Do something with the error here
+      console.log("ERR: " + err);
+    });
+
+
+
+
 }]);
 
 
@@ -50,23 +50,23 @@ $(document).ready(function () {
   var counter = 0;
 
   $("#addrow").on("click", function () {
-      var newRow = $("<tr>");
-      var cols = "";
+    var newRow = $("<tr>");
+    var cols = "";
 
-      cols += '<td><input type="text" class="form-control text-center no-border" placeholder="Name"' + counter + '"/></td>';
-      cols += '<td><input type="text" class="form-control text-center no-border" placeholder="Email"' + counter + '"/></td>';
-      cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger " value="Cancel"></td>';
+    cols += '<td><input type="text" class="form-control text-center no-border" placeholder="Name"' + counter + '"/></td>';
+    cols += '<td><input type="text" class="form-control text-center no-border" placeholder="Email"' + counter + '"/></td>';
+    cols += '<td><button class="ibtnDel btn btn-md btn-link-danger "><i class="material-icons" style="color:red;">remove</i></button></td>';
 
-      newRow.append(cols);
-      $("table.order-list").append(newRow);
-      counter++;
+    newRow.append(cols);
+    $("table.order-list").append(newRow);
+    counter++;
   });
 
 
 
   $("table.order-list").on("click", ".ibtnDel", function (event) {
-      $(this).closest("tr").remove();       
-      counter -= 1
+    $(this).closest("tr").remove();
+    counter -= 1
   });
 
 
