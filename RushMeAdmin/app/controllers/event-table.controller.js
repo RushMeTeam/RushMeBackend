829ccbfe9ae17af10f347ae58ec6203064598a4e
Adam <adamthk@gmail.com>
2019-03-28 16:49:31 -0400

    Replaces all vars with lets

--- RushMeAdmin/app/controllers/event-table.controller.js
+++ RushMeAdmin/app/controllers/event-table.controller.js
@@ -3,7 +3,7 @@
 
  angular.module('RushMeAdminControllers').controller('EventTableCtrl', ['$scope', '$http', function ($scope, $http) {  	
   $scope.events = [];
-  
+
   $http.get("/in/events/").then(
     function(res){
       $scope.events = res.data;
