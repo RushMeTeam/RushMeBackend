041be258ee7abfab97955550902f63f630d23c5f
Kevin Porrecca <kporrecca@gmail.com>
2019-04-19 17:34:20 -0400

    corrected file name on banner.controller.js

--- RushMeAdmin/app/controllers/banner.controller.js
+++ RushMeAdmin/app/controllers/banner.controller.js
@@ -1,5 +1,5 @@
-// File: frat-table.controller.js
-// Description: frat-table controller that will handle all logic for the frat-table.html page
+// File: banner.controller.js
+// Description: banner controller that will handle all logic for the banner.html and banner-portal.html pages
 
 angular.module('RushMeAdminControllers').controller('BannerCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.email;
